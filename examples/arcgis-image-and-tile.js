4d46aad72486133e86e79a195da6c4288fd522c8
zhwy <498971093@qq.com>
2020-08-04 18:45:36 +0800

    指定投影

--- examples/arcgis-image-and-tile.js
+++ examples/arcgis-image-and-tile.js
@@ -2,11 +2,12 @@ import Map from '../src/ol/Map.js';
 import View from '../src/ol/View.js';
 import { ImageArcGISRest, OSM } from '../src/ol/source.js';
 import { Image as ImageLayer, Tile as TileLayer } from '../src/ol/layer.js';
-import { fromLonLat } from "../src/ol/proj.js";
+import { fromLonLat, Projection } from "../src/ol/proj.js";
 
 const url =
   'http://10.10.20.46:6080/arcgis/rest/services/WaterWay_2017/MapServer';
-const url2 = ""
+// const url2 = 'https://sampleserver1.arcgisonline.com/ArcGIS/rest/services/' +
+//   'Specialty/ESRI_StateCityHighway_USA/MapServer';
 
 const layers = [
   // new TileLayer({
@@ -16,11 +17,14 @@ const layers = [
     source: new ImageArcGISRest({
       ratio: 1,
       params: {
-        BBOXSR:2385,
-        IMAGESR:2385
+        BBOXSR: 2385,
+        IMAGESR: 2385
       },
-      url: url,
-      projection: "EPSG:2385"
+      url: url2,
+      projection: new Projection({
+        code: "EPSG:2385",
+
+      })
     }),
   }),
 ];
@@ -29,7 +33,7 @@ const map = new Map({
   target: 'map',
   view: new View({
     // center: fromLonLat([118.670, 39.083]),
-    center:[-10210,29508],
+    center: [-10210, 29508],
     zoom: 10,
     projection: "EPSG:3857"
   }),
