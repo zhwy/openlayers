f36487eee6daae1449a0de00ded337ad8b0162ec
zhwy <498971093@qq.com>
2023-04-16 14:29:08 +0800

    geoserver矢量切片例子

--- examples/vector-tiles-geoserver.js
+++ examples/vector-tiles-geoserver.js
@@ -4,111 +4,99 @@ import GeoJSON from "../src/ol/format/GeoJSON.js";
 import TileLayer from "../src/ol/layer/Tile.js";
 import VectorTileLayer from "../src/ol/layer/VectorTile.js";
 import VectorTile from "../src/ol/source/VectorTile.js";
-import XYZ from "../src/ol/source/XYZ.js";
+import OSM from "../src/ol/source/OSM.js";
 import Projection from "../src/ol/proj/Projection.js";
 import TileGrid from "../src/ol/tilegrid/WMTS.js";
+import { Fill, Stroke, Style, Circle } from "../src/ol/style.js";
 
-const proj = new Projection({
-  code: "EPSG:4326",
-  units: "degrees"
-});
-var resolutions = [
-  0.703125,
-  0.3515625,
-  0.17578125,
-  0.087890625,
-  0.0439453125,
-  0.02197265625,
-  0.010986328125,
-  0.0054931640625,
-  0.00274658203125,
-  0.001373291015625,
-  6.866455078125e-4,
-  3.4332275390625e-4,
-  1.71661376953125e-4,
-  8.58306884765625e-5,
-  4.291534423828125e-5,
-  2.1457672119140625e-5,
-  1.0728836059570312e-5,
-  5.364418029785156e-6,
-  2.682209014892578e-6,
-  1.341104507446289e-6,
-  6.705522537231445e-7,
-  3.3527612686157227e-7
-];
-const gridsetName = "EPSG:4326";
-const gridNames = [
-  "EPSG:4326:0",
-  "EPSG:4326:1",
-  "EPSG:4326:2",
-  "EPSG:4326:3",
-  "EPSG:4326:4",
-  "EPSG:4326:5",
-  "EPSG:4326:6",
-  "EPSG:4326:7",
-  "EPSG:4326:8",
-  "EPSG:4326:9",
-  "EPSG:4326:10",
-  "EPSG:4326:11",
-  "EPSG:4326:12",
-  "EPSG:4326:13",
-  "EPSG:4326:14",
-  "EPSG:4326:15",
-  "EPSG:4326:16",
-  "EPSG:4326:17",
-  "EPSG:4326:18",
-  "EPSG:4326:19",
-  "EPSG:4326:20",
-  "EPSG:4326:21"
-];
+const use3857 = true;
+
+const proj = use3857
+  ? new Projection({
+      code: "EPSG:3857",
+      units: "m",
+    })
+  : new Projection({
+      code: "EPSG:4326",
+      units: "degrees",
+    });
+
+const extent = use3857
+  ? [-20037508.34, -20037508.34, 20037508.34, 20037508.34]
+  : [-180.0, -90.0, 180.0, 90.0];
+
+const maxResolution = use3857 ? (20037508.34 * 2) / 256 : 180 / 256;
+
+const resolutions = [];
+
+for (let i = 0; i < 20; i += 1) {
+  resolutions.push(maxResolution / Math.pow(2, i));
+}
+
 const tilegrid = new TileGrid({
   tileSize: [256, 256],
-  origin: [-180.0, 90.0],
+  origin: [extent[0], extent[3]],
   resolutions: resolutions,
-  matrixIds: gridNames,
-  extent: [-180.0, -90.0, 180.0, 90.0]
+  extent: extent,
 });
 
 const map = new Map({
   target: "map",
   view: new View({
-    center: [114.026069, 22.626171],
-    zoom: 10,
-    projection: proj
+    center: [0, 0],
+    zoom: 1,
+    projection: proj,
+    constrainResolution: true,
   }),
   layers: [
     new TileLayer({
-      source: new XYZ({
-        // projection: proj,
-        url:
-          "http://wprd0{1-4}.is.autonavi.com/appmaptile?lang=zh_cn&size=1&style=7&x={x}&y={y}&z={z}"
-      })
+      source: new OSM(),
     }),
+    // tileGrid需和geoserver上匹配
     new VectorTileLayer({
       source: new VectorTile({
         projection: proj,
         tileGrid: tilegrid,
-        format: new GeoJSON(),
-        url:
-          "http://localhost:8089/geoserver/gwc/service/tms/1.0.0/shenzhen_gismod%3AGCJPM_LINE_AXISES2@EPSG%3A4326@geojson/{z}/{x}/{-y}.geojson"
-        // url:
-        //   "http://localhost:8089/geoserver/gwc/service/wmts?REQUEST=GetTile&SERVICE=WMTS&VERSION=1.0.0&LAYER=shenzhen_gismod:GCJPM_LINE_AXISES2&STYLE=&TILEMATRIX=EPSG:900913:{z}&TILEMATRIXSET=EPSG:4326&FORMAT=application/json;type=geojson&TILECOL={x}&TILEROW={y}"
-      })
-    })
-  ]
+        format: new GeoJSON({
+          dataProjection: proj,
+        }),
+        // url: `http://localhost:8080/geoserver/gwc/service/tms/1.0.0/ForTest:PointTest@EPSG:${
+        //   use3857 ? "3857" : "4326"
+        // }@geojson/{z}/{x}/{-y}.geojson`,
+        url: `http://localhost:8080/geoserver/gwc/service/wmts?REQUEST=GetTile&SERVICE=WMTS&VERSION=1.0.0&LAYER=ForTest:PointTest&STYLE=&TILEMATRIX=EPSG:${
+          use3857 ? "3857" : "4326"
+        }:{z}&TILEMATRIXSET=EPSG:${
+          use3857 ? "3857" : "4326"
+        }&FORMAT=application/json;type=geojson&TILECOL={x}&TILEROW={y}`,
+      }),
+      style: (fea) => {
+        return new Style({
+          image: new Circle({
+            radius: 3,
+            fill: new Fill({
+              color: "red",
+            }),
+          }),
+        });
+      },
+    }),
+  ],
 });
 
-// map.on("pointermove", showInfo);
+map.on("pointermove", showInfo);
 
 const info = document.getElementById("info");
 function showInfo(event) {
-  const features = map.getFeaturesAtPixel(event.pixel);
-  if (features.length == 0) {
-    info.innerText = "";
-    info.style.opacity = 0;
-    return;
-  }
-  const properties = features[0].getProperties();
-  info.innerText = JSON.stringify(properties, null, 2);
-  info.style.opacity = 1;
+  info.innerHTML = `${event.coordinate[0].toFixed(
+    2
+  )},<br/>${event.coordinate[1].toFixed(2)}`;
+  // const features = map.getFeaturesAtPixel(event.pixel);
+  // if (features.length == 0) {
+  //   info.innerText = "";
+  //   info.style.opacity = 0;
+  //   return;
+  // }
+  // const properties = features[0].getProperties();
+  // info.innerText = JSON.stringify(properties, null, 2);
+  // info.style.opacity = 1;
 }
